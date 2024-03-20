1ea92e9252c26326fd38892ae9e07fa1f4ccb5ce
itsdanica <76499075+itsdanica@users.noreply.github.com>
2024-03-20 10:11:44 -0500

    added api and began making basic search engine

--- src/script.js
+++ src/script.js
@@ -1,8 +1,31 @@
-function changeCityName(event) {
+function changeWeather(response) {
+  let heading = document.querySelector("#heading");
+  let temp = document.querySelector(".temp-number");
+  // let day = document.querySelector("#day");
+  // let hour = document.querySelector("#hour");
+  // let minute = document.querySelector("#minute");
+  let sky = document.querySelector("#sky");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  console.log(response);
+  heading.innerHTML = response.data.city;
+  temp.innerHTML = Math.round(response.data.temperature.current);
+  sky.innerHTML = response.data.condition.description;
+  console.log(`sky: ${response.data.condition.description}`);
+  humidity.innerHTML = response.data.temperature.humidity;
+  console.log(`humidity: ${response.data.temperature.humidity}`);
+  windSpeed.innerHTML = response.data.wind.speed;
+  console.log(`wind speed:${response.data.wind.speed}`);
+}
+function searchCity(city) {
+  let apiKey = `d804a6ef932oaf3dbf673f68a8ff6cta`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(changeWeather);
+}
+function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#enter-city");
-  let heading = document.querySelector("#heading");
-  heading.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchCityForm = document.querySelector("#search-city-form");
-searchCityForm.addEventListener("submit", changeCityName);
+searchCityForm.addEventListener("submit", submitCity);
